f91eb7b32f33edb805d898d7e662751b9174b36d
Yuriy Martini <y.martini@soluzionesoftware.com>
2020-07-24 12:34:14 +0200

    init

---
+++ src/ServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace SoluzioneSoftware\PackageName;
+
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+
+class ServiceProvider extends BaseServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap the application services.
+     */
+    public function boot()
+    {
+        //
+    }
+}
